d91aa28ce5a616365ba578d62901e470fd7ed3cc
Qian Jin <jinqian.hz@gmail.com>
2017-10-02 10:38:35 +0200

    [reader] add open reader button

--- app/src/main/java/fr/xebia/pocribreader/MainActivity.kt
+++ app/src/main/java/fr/xebia/pocribreader/MainActivity.kt
@@ -1,12 +1,19 @@
 package fr.xebia.pocribreader
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import fr.xebia.pocribreader.reader.OcrCaptureActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_open_reader.setOnClickListener {
+            startActivity(Intent(this, OcrCaptureActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/fr/xebia/pocribreader/reader/OcrCaptureActivity.kt
@@ -0,0 +1,13 @@
+package fr.xebia.pocribreader.reader
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import fr.xebia.pocribreader.R
+
+class OcrCaptureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ocr_capture)
+    }
+}
