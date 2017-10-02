80a1a6e28514b99533189474159ea3a615229572
Benjamin LACROIX <benjlacroix@gmail.com>
2017-10-02 11:12:04 +0200

    #5 Set Rib

--- app/src/main/java/fr/xebia/pocribreader/result/BankAccountInfoActivity.kt
+++ app/src/main/java/fr/xebia/pocribreader/result/BankAccountInfoActivity.kt
@@ -3,6 +3,7 @@ package fr.xebia.pocribreader.result
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import fr.xebia.pocribreader.R
+import kotlinx.android.synthetic.main.bank_account_info_activity.*
 
 class BankAccountInfoActivity : AppCompatActivity() {
 
@@ -15,6 +16,10 @@ class BankAccountInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bank_account_info_activity)
+
         bankAccount = intent.getParcelableExtra<BankAccount>(BANK_ACCOUNT_PARAM)
+
+        bankAccountInfoIban.text = bankAccount.iban
     }
+
 }
